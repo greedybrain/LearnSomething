0ccdd247f3e86cc8ec2c9f67bcb0fd86322621d4
greedybrain <greedybrain18@gmail.com>
2020-11-09 15:38:03 -0500

    ...

--- Node/Building-RESTful-APIs-Using-Express/MovieApp/routes/customers.js
+++ Node/Building-RESTful-APIs-Using-Express/MovieApp/routes/customers.js
@@ -12,11 +12,11 @@ const express = require('express');
 const customersRouter = express.Router()
 
 //! get all genres
-customersRouter.get('/', async (req, res) => getCustomers(res))
-customersRouter.get('/:id', async (req, res) => getCustomer(req, res))
-customersRouter.post('/', async (req, res) => createCustomer(req, res))
-customersRouter.put('/:id', async (req, res) => updateCustomer(req, res))
-customersRouter.delete('/:id', async (req, res) => deleteCustomer(req, res))
+customersRouter.get('/', async (req, res) => await getCustomers(res))
+customersRouter.get('/:id', async (req, res) => await getCustomer(req, res))
+customersRouter.post('/', async (req, res) => await createCustomer(req, res))
+customersRouter.put('/:id', async (req, res) => await updateCustomer(req, res))
+customersRouter.delete('/:id', async (req, res) => await deleteCustomer(req, res))
 
 //! Export router to use in index.js file 
 module.exports = customersRouter
--- Node/Building-RESTful-APIs-Using-Express/MovieApp/routes/genres.js
+++ Node/Building-RESTful-APIs-Using-Express/MovieApp/routes/genres.js
@@ -12,11 +12,11 @@ const express = require('express');
 const genresRouter = express.Router()
 
 //! get all genres
-genresRouter.get('/', async (req, res) => getGenres(res))
-genresRouter.get('/:id', async (req, res) => getGenre(req, res))
-genresRouter.post('/', async (req, res) => createGenre(req, res))
-genresRouter.put('/:id', async (req, res) => updateGenre(req, res))
-genresRouter.delete('/:id', async (req, res) => deleteGenre(req, res))
+genresRouter.get('/', async (req, res) => await getGenres(res))
+genresRouter.get('/:id', async (req, res) => await getGenre(req, res))
+genresRouter.post('/', async (req, res) => await createGenre(req, res))
+genresRouter.put('/:id', async (req, res) => await updateGenre(req, res))
+genresRouter.delete('/:id', async (req, res) => await deleteGenre(req, res))
 
 //! Export router to use in index.js file 
 module.exports = genresRouter
