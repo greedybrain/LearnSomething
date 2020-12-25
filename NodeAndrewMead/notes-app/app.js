45d6f53a42cc7cd681f4bdc87d3f01a215f53734
greedybrain <greedybrain18@gmail.com>
2020-12-25 14:12:08 -0500

    ...

--- NodeAndrewMead/notes-app/app.js
+++ NodeAndrewMead/notes-app/app.js
@@ -1,11 +1,4 @@
-//! Core Modules
-const fs = require('fs');
+const getNotes = require('./notes')
 
-const file = 'notes.txt'
-const msgToBeAppended = ' this will be appended to the whatever is in the notes.txt file'
-
-//! Write file, also OVERWRITES 
-// fs.writeFileSync('notes.txt', "and in additon we have THIS")
-
-//! Appends to exisiting file, doesn't OVERWRITE
-fs.appendFileSync(file, msgToBeAppended)
+const message = getNotes()
+console.log(message)
