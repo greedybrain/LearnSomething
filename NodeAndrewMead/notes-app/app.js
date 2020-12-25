2b72d954fb0dd49e0a89c3ecb049aab1e02151f9
greedybrain <greedybrain18@gmail.com>
2020-12-25 14:34:24 -0500

    ...

--- NodeAndrewMead/notes-app/app.js
+++ NodeAndrewMead/notes-app/app.js
@@ -1,4 +1,10 @@
+//! NPM Modules
+const validator = require('validator');
+
 const getNotes = require('./notes')
 
 const message = getNotes()
 console.log(message)
+
+// console.log(validator.isEmail('willis@gmail.com'))
+console.log(validator.isURL('https://google.co'))
