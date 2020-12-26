56ccbcbc94bf09a337b5e7c5eb3ca54d1b72f323
greedybrain <greedybrain18@gmail.com>
2020-12-26 00:13:24 -0500

    ...

--- NodeAndrewMead/notes-app/app.js
+++ NodeAndrewMead/notes-app/app.js
@@ -1,10 +1,21 @@
 //! NPM Modules
 const validator = require('validator');
+const chalk = require('chalk');
 
+//! Custom Modules/Helper Vars
 const getNotes = require('./notes')
+//==============================
+const error = chalk.bold.red.inverse
+const success = chalk.bold.green.inverse
+const log = console.log
 
+// 
 const message = getNotes()
 console.log(message)
 
 // console.log(validator.isEmail('willis@gmail.com'))
-console.log(validator.isURL('https://google.co'))
+if (validator.isURL('https://google.co')) log(success("Valid url"))
+else log(error("Please check URL"))
+
+
+// episode 17 heroes
