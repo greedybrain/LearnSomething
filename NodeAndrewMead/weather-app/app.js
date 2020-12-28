f57ada94ad4bbcb50532efbda1d17e1904b007ff
greedybrain <greedybrain18@gmail.com>
2020-12-28 17:33:15 -0500

    ...

--- NodeAndrewMead/weather-app/app.js
+++ NodeAndrewMead/weather-app/app.js
@@ -1,7 +1,26 @@
-console.log("Starting")
+//! NPM Modules
+require('dotenv').config()
+const chalk = require('chalk')
 
-setTimeout(() => {
-        console.log("2 second timer")
-}, 2000);
+//! Custom Modules
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast')
 
-console.log("Stopping")
+//! Custom Vars
+const error = chalk.redBright.bold.inverse
+const success = chalk.greenBright.bold.inverse
+const log = console.log
+
+const location = process.argv[2]
+if (!location) log(error("No location was provided"))
+else{ 
+        geocode(location, (err, { lat, long, location } = {}) => {
+                if (err) return log(error(err))
+                forecast(lat, long, (err, { temperature, feelslike }) => {
+                        if (err) return log(error(err))
+                        log(success(`It is now ${ temperature }˚ in ${ location }, but it really feels like ${ feelslike }˚.`))
+                })
+        })
+}  
+
+
---
+++ NodeAndrewMead/web-server/src/app.js
@@ -0,0 +1,5 @@
+//! NPM Modules
+const express = require('express');
+
+//! Custom Vars
+const app = express()
